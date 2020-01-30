b890fb0bd3f98438d79c93a3b3c1594d2fd2c650
arjun289 <arjun289singh@gmail.com>
2020-01-30 12:11:35 +0800

    Core Functions response, template.

--- mastery/lib/mastery/core/question.ex
+++ mastery/lib/mastery/core/question.ex
@@ -3,6 +3,21 @@ defmodule Mastery.Core.Question do
   Module to define struct for a question based on different
   templates.
   """
+  alias Mastery.Core.Template
 
   defstruct ~w[asked questions template]a
+
+  ################### private functions ###############
+
+  defp build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
 end
--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -4,4 +4,24 @@ defmodule Mastery.Core.Template do
   """
 
   defstruct ~w[name category instructions raw compiled generators checker]a
+
+  @typedoc """
+  `name`: name of the template.
+  `category`:
+  `instructions`: instructions for the user to how to approach the problems
+                created using the template.
+  `raw`: code to generate the questions from this template.
+  `compiled`:
+  `generators`: would be used to fill in the substitutions.
+  `checker`: checker function to check for correct answer.
+  """
+
+  @type t :: %__MODULE__{}
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+    struct!(__MODULE__, Keyword.put(fields, :compiled, EEx.compile_string(raw)))
+  end
+
+
 end
