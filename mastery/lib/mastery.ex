48d8f2f7e8f7a1419bf56cc77bf8e8af3c94ff40
arjun289 <arjun289singh@gmail.com>
2020-06-15 09:41:16 +0800

    Add boundary and validation for quiz mastery
    - Add validation layer for user inputs
    - Add quiz session module to handle user sessions
    - Add public API to interact with quizzes

--- mastery/lib/mastery.ex
+++ mastery/lib/mastery.ex
@@ -2,17 +2,44 @@ defmodule Mastery do
   @moduledoc """
   Documentation for Mastery.
   """
+  alias Mastery.Boundary.{QuizManager,
+    QuizSession,
+    TemplateValdiator,
+    QuizValidator
+  }
+  alias Mastery.Core.Quiz
 
-  @doc """
-  Hello world.
+  def statrt_quiz_manager() do
+    GenServer.start_link(QuizManager, %{}, name: QuizManager)
+  end
 
-  ## Examples
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+      :ok <- GenServer.call(QuizManager, {:build_quiz, fields}),
+    do: :ok, else: (error -> error)
+  end
 
-      iex> Mastery.hello()
-      :world
+  def add_template(title, fields) do
+    with :ok <- TemplateValdiator.errors(fields),
+      :ok <- GenServer.call(QuizManager, {:add_template, title, fields}),
+    do: :ok, else: (error -> error)
+  end
 
-  """
-  def hello do
-    :world
+  def take_quiz(title, email) do
+    with %Quiz{}=quiz <- QuizManager.lookup_quiz_by_title(QuizManager, title),
+      {:ok, session} <- GenServer.start_link(QuizSession, {quiz, email})
+    do
+      session
+    else
+      error -> error
+    end
+  end
+
+  def select_question(session) do
+    QuizSession.select_question(session)
+  end
+
+  def answer_question(session, answer) do
+    QuizSession.answer_question(session, answer)
   end
 end
---
+++ mastery/lib/mastery/boundary/validator.ex
@@ -0,0 +1,38 @@
+defmodule Mastery.Boundary.Validator do
+
+  def require(errors, fields, field_name, validator) do
+    present = Map.has_key?(fields, field_name)
+    check_required_field(present, fields, errors, field_name, validator)
+  end
+
+  def optional(errors, fields, field_name, validator) do
+    if Map.has_key?(fields, field_name) do
+      require(errors, fields, field_name, validator)
+    else
+      errors
+    end
+  end
+
+  def check(true = _valid, _message), do: :ok
+  def check(false = _valid, message), do: message
+
+  ############### private functions ############
+
+  defp check_required_field(true = _present, fields, errors, field_name,
+      _valdiator_fn = f) do
+    valid = fields |> Map.fetch!(field_name) |> f.()
+    check_field(valid, errors, field_name)
+  end
+  defp check_required_field(_present, _field, errors, field_name, _f) do
+    [{field_name, "is required"} | errors]
+  end
+
+  defp check_field(:ok, _errors, _field_name), do: :ok
+  defp check_field({:error, message}, errors, field_name) do
+    [{field_name, message} | errors]
+  end
+  defp check_field({:errors, messages}, errors, field_name) do
+    errors ++ Enum.map(messages, &{field_name, &1})
+  end
+
+end
